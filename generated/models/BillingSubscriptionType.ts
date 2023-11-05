769f1ab30748763f9e0cf215752553c936ed3c8b
paambaati <paambaati@users.noreply.github.com>
2023-11-05 00:59:27 +0000

    feat: update to track latest spec changes

--- generated/models/BillingSubscriptionType.ts
+++ generated/models/BillingSubscriptionType.ts
@@ -8,4 +8,4 @@
  * Notice that for users without billing account this will be "UNKNOWN"
  *
  */
-export type BillingSubscriptionType = 'UNKNOWN' | 'free' | 'pro' | 'direct_sales';
+export type BillingSubscriptionType = 'UNKNOWN' | 'free' | 'pro' | 'direct_sales' | 'aws_marketplace';
