d02f771491aeaa6a72d4caa4140148f83c1ccc86
paambaati <paambaati@users.noreply.github.com>
2024-08-04 01:09:14 +0000

    feat: update to track latest spec changes

--- generated/models/EndpointType.ts
+++ generated/models/EndpointType.ts
@@ -4,7 +4,6 @@
 /* eslint-disable */
 /**
  * The compute endpoint type. Either `read_write` or `read_only`.
- * The `read_only` compute endpoint type is not yet supported.
  *
  */
 export type EndpointType = 'read_only' | 'read_write';
